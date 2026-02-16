4ca5368b370890327419414218435177c7d6fe8e
Jatin-Joshi-9 <jatinjoshi090803@gmail.com>
2026-02-16 19:11:43 +0530

    chore: add the constraint annotation on title attributes

--- src/main/java/com/project/learntestandcode/dto/CreateBookRequest.java
+++ src/main/java/com/project/learntestandcode/dto/CreateBookRequest.java
@@ -1,8 +1,11 @@
 package com.project.learntestandcode.dto;
 
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 
 @Getter
 public class CreateBookRequest {
+    @NotBlank(message = "Title can't be a blank'")
+    @Size(max = 100, message = "Title can't exceed 100 characters")
     String title;
 }
